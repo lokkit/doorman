7d2024a4fc866aeab32e232ba3ddc9cb558f548c
Andreas Schmid <ikeark@gmail.com>
2017-04-19 23:09:24 +0200

    Verify whisper message sender and prepare execution of command

--- lokkit_doorman/lokkit_doorman.py
+++ lokkit_doorman/lokkit_doorman.py
@@ -2,6 +2,8 @@
 
 import logging
 import logging.handlers
+import json
+from collections import OrderedDict
 from time import sleep
 from ethjsonrpc import EthJsonRpc
 
@@ -26,14 +28,61 @@ def main():
     except:
         logger.error('Check if connected node has shh enabled')
 
+    # config
+    contract_address = '0x58b5e51386b46d018dcd0a3db91a6c69fed20ea8'
+    description = c.call(contract_address, 'description()', [], ['string'])[0]
+    deposit = c.call(contract_address, 'deposit()', [], ['uint256'])[0]
+    location = c.call(contract_address, 'location()', [], ['string'])[0]
+    pricePerTime = c.call(contract_address, 'pricePerTime()', [], ['uint256'])[0]
+    current_renter = c.call(contract_address, 'currentRenter()', [], ['address'])[0]
+    logger.info('Configured rentable contract {0}\n\
+  description: {1}\n\
+  location: {2}\n\
+  deposit: {3}\n\
+  pricePerTime: {4}\n\
+  current_renter: {5}'.format(contract_address, description, location,
+                  deposit, pricePerTime, current_renter))
 
+    # TODO: implement correct filter
     filter_id = c.shh_newFilter("", [None])
+    logger.info('Listen for incomming messages..')
     try:
         while True:
-            print('Check for messages:')
-            print c.shh_getFilterChanges(filter_id)
-            # TODO: ecrecver not possible? Maybe do it in contract?
-            # api documentation: https://github.com/ethereum/wiki/wiki/JSON-RPC#shh_newfilter
+            msg = c.shh_getFilterChanges(filter_id)
+            for m in msg:
+                # TODO: Validate message, make sure its payload is correct, etc
+                logger.debug('Message details:\n  from {0}\n  ttl {1}\n  payload: {2}'.format(m['from'], m['ttl'], m['payload']))
+
+                payload = None
+                try:
+                    payload = json.loads(m['payload'][2:].decode('hex'), object_pairs_hook=OrderedDict) # payload contains digest and message
+                except:
+                    logger.error('Error parsing whisper message: {0}', m)
+                    continue
+
+                signature = payload['digest'] # hex value starting with 0x....
+                message = payload['message']  # dict object
+                logger.debug('signature: {0}\nmessage {1}'.format(signature, message))
+
+                # get ethereum address of the sender
+                message_json = json.dumps(message, separators=(',', ':')) # separators: no whitespaces
+                message_hex_string = '0x{0}'.format(message_json.encode('hex'))
+                logger.debug('message_json_string {0}\nmessage_json_string_hex {1}'.format(message_json, message_hex_string))
+
+                address_of_sender = c.personal_ecRevoer(message_hex_string, signature)
+
+                logger.info('Command "{0}", rentableAddress "{1}"'.format(message['command'], message['rentableAddress']))
+                logger.info('ethereum account (recovered) "{0}"'.format(address_of_sender))
+
+                current_renter = c.call(contract_address, 'currentRenter()', [], ['address'])[0]
+
+                if address_of_sender[2:] != current_renter:
+                    logger.error('Refuse execution of command "{0}": requester ({1}) is not the current renter ({2}) of rentable "{3}"'
+                            .format(message['command'], address_of_sender, current_renter, message['rentableAddress']))
+                    continue
+
+                logger.info('Executing command "{0}" for rentable {1}'.format(message['command'], message['rentableAddress']))
+
             sleep(1) # sleep 1 second
     except KeyboardInterrupt:
         pass
