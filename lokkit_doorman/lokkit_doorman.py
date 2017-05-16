f25125d2aadbb720d1c322135cbce06da02c563c
Dominik Hirzel <hirzeldominik@gmail.com>
2017-05-16 15:24:12 +0200

    Update for Whisperv5

--- lokkit_doorman/lokkit_doorman.py
+++ lokkit_doorman/lokkit_doorman.py
@@ -8,6 +8,7 @@ import json
 import subprocess
 import yaml
 
+import sha3
 from collections import OrderedDict
 from time import sleep
 from ethjsonrpc import EthJsonRpc
@@ -15,7 +16,7 @@ from ethjsonrpc.exceptions import ConnectionError
 
 # Setup logger
 logger = logging.getLogger('lokkitLogger')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 if (os.name == 'posix'):
 	logger.addHandler(logging.handlers.SysLogHandler(address = '/dev/log'))
 logger.addHandler(logging.StreamHandler()) # log to console
@@ -106,6 +107,7 @@ def main():
     port = config['node_rpc_port']
     rentable_address = config['rentable_address']
     script = config['script']
+    symmetric_key_password = config['symmetric_key_password']
 
     logger.info('Connecting to {0}:{1}'.format(host, port))
     c = EthJsonRpc(host, port)
@@ -137,57 +139,60 @@ def main():
         logger.error('Address {0} is not a Rentable'.format(rentable_address))
         return
 
-    # TODO: implement correct filter
-    filter_id = c.shh_newFilter("", ['rentable'])
+    symmetric_key_address = c.shh_addSymmetricKeyFromPassword(symmetric_key_password)
+    symmetric_key = c.shh_getSymmetricKey(symmetric_key_address)
+    topic = c.web3_sha3(rentable_address)[:10]
+
+    filter_id = c.shh_subscribe(type='sym', key=symmetric_key_address, sig=None, minPow=None, topics=[topic])
     logger.info('Listen for incomming messages..')
     try:
         while True:
-            msg = c.shh_getFilterChanges(filter_id)
-            for m in msg:
-                # TODO: Validate message, make sure its payload is correct, etc
-                logger.debug('Message details:\n  from {0}\n  ttl {1}\n  payload: {2}'
-                        .format(m['from'], m['ttl'], m['payload']))
+            messages = c.shh_getNewSubscriptionMessages(filter_id)
+            for message in messages:
+                logger.debug('Message details:\n  hash {0}\n  ttl {1}\n  payload: {2}\n  topic: {2}'
+                        .format(message['hash'], message['ttl'], message['payload'], message['topic']))
 
                 payload = None
                 try:
                     # payload contains digest and message
-                    payload = json.loads(m['payload'][2:].decode('hex'), object_pairs_hook=OrderedDict)
+                    message_payload_string = message['payload'][2:].decode('hex')
+                    payload = json.loads(message_payload_string, object_pairs_hook=OrderedDict)
                 except:
-                    logger.error('Error parsing whisper message: {0}', m)
+                    logger.error('Error parsing whisper message payload: {0}', message['payload'])
                     continue
 
                 signature = payload['digest'] # hex value starting with 0x....
-                message = payload['message']  # dict object
-                logger.debug('signature: {0}\nmessage {1}'.format(signature, message))
+                lokkit_message = payload['message']  # dict object
+                logger.debug('signature: {0}\nlokkit_message {1}'.format(signature, lokkit_message))
 
                 # get ethereum address of the sender
-                message_json = json.dumps(message, separators=(',', ':')) # separators: no whitespaces
+                message_json = json.dumps(lokkit_message, separators=(',', ':')) # separators: no whitespaces
                 message_hex_string = '0x{0}'.format(message_json.encode('hex'))
                 logger.debug('message_json_string {0}\nmessage_json_string_hex {1}'
                         .format(message_json, message_hex_string))
 
                 signer = c.personal_ecRevoer(message_hex_string, signature)
-                sender = message['whisperIdentity']
+                key = lokkit_message['key']
 
-                logger.info('Command "{0}", rentableAddress "{1}", whisperIdentity "{2}"'
-                    .format(message['command'], message['rentableAddress'], sender))
-                logger.info('ethereum account (recovered) "{0}"'
+                logger.info('command "{0}", rentableAddress "{1}", key "{2}"'
+                    .format(lokkit_message['command'], lokkit_message['rentableAddress'], key))
+                logger.info('signing ethereum account (recovered) "{0}"'
                     .format(signer))
 
                 current_renter = c.call(rentable_address, 'currentRenter()', [], ['address'])[0]
+                logger.info('current renter (smart contract)      "{0}"'
+                    .format(signer))
 
                 if signer[2:] != current_renter:
                     logger.error('Refuse execution of command "{0}": requester ({1}) is not the current renter ({2}) of rentable "{3}"'
-                            .format(message['command'], signer, current_renter, message['rentableAddress']))
+                            .format(lokkit_message['command'], signer, current_renter, lokkit_message['rentableAddress']))
                     continue
-                if sender != m['from']:
-                    logger.error('Encrypted whisper identity is not of the sender. Possible replay attack detected')
+                if key != message['sig']:
+                    logger.error('Encrypted public key is not of the sender. Possible replay attack detected')
                     continue
 
-                logger.info('Executing script "{0}" with argument "{1}" for rentable "{2}"'.format(script, message['command'], message['rentableAddress']))
-
-                n = 0 if message['command'] == 'lock' else 1
-                subprocess.call([script, message['command']])
+                logger.info('Executing script "{0}" with argument "{1}" for rentable "{2}"'.format(script, lokkit_message['command'], lokkit_message['rentableAddress']))
+                subprocess.call([script, lokkit_message['command']])
 
             sleep(1) # sleep 1 second
     except KeyboardInterrupt:
